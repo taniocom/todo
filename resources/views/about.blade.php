92fb2bbdc81565466efb03cf85339020903307b5
Boontan Rithitraipop <taniocom@gmail.com>
2018-12-16 14:42:06 +0700

    add master template

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,13 +1,6 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<h1>About Us</h1>
-</body>
-</html>
+@extends('layout.master')
+@section('title','หน้าหลัก')
+@section('content')
+    <h1>About</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet cupiditate eligendi ex facere facilis odio porro provident vero! Assumenda debitis libero obcaecati placeat possimus rem reprehenderit similique suscipit. Ea, sint.</p>
+@endsection
